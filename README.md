cf742ee0c75c96d69c31efc6e595cd089b727b61
Francois Beaufort <beaufort.francois@gmail.com>
2018-04-03 17:01:19 +0200

    Export to CSV by default

--- README.md
+++ README.md
@@ -12,18 +12,10 @@ I wanted to be able to read VideoDecodeStats LevelDB database from Chrome OS so.
 
 ```
 $ node main.js
-12|3840x2160|30  :  DecodeStatsProto {
-  framesDecoded: Long { low: 127, high: 0, unsigned: true },
-  framesDropped: Long { low: 63, high: 0, unsigned: true },
-  framesDecodedPowerEfficient: Long { low: 0, high: 0, unsigned: true } }
-12|7680x4320|25  :  DecodeStatsProto {
-  framesDecoded: Long { low: 91, high: 0, unsigned: true },
-  framesDropped: Long { low: 73, high: 0, unsigned: true },
-  framesDecodedPowerEfficient: Long { low: 0, high: 0, unsigned: true } }
-12|7680x4320|30  :  DecodeStatsProto {
-  framesDecoded: Long { low: 519, high: 0, unsigned: true },
-  framesDropped: Long { low: 352, high: 0, unsigned: true },
-  framesDecodedPowerEfficient: Long { low: 0, high: 0, unsigned: true } }
+profileId	profile	resolution	frameRate	framesDecoded	framesDropped	framesDecodedPowerEfficient
+12	VP9PROFILE_PROFILE0	3840x2160	30	127	63	0
+12	VP9PROFILE_PROFILE0	7680x4320	25	91	73	0
+12	VP9PROFILE_PROFILE0	7680x4320	30	519	352	0
 ```
 
 Then I decided I wanted to write my own data into this database so...
--- main.js
+++ main.js
@@ -19,13 +19,54 @@ const db = levelup(leveldown(args.dir))
 const readline = require('readline')
 const stream =  require('stream')
 
+// Extracted from https://chromium.googlesource.com/chromium/src/media/+/master/base/video_codecs.h#49
+const profiles = {
+  0: 'H264PROFILE_BASELINE',
+  1: 'H264PROFILE_MAIN',
+  2: 'H264PROFILE_EXTENDED',
+  3: 'H264PROFILE_HIGH',
+  4: 'H264PROFILE_HIGH10PROFILE',
+  5: 'H264PROFILE_HIGH422PROFILE',
+  6: 'H264PROFILE_HIGH444PREDICTIVEPROFILE',
+  7: 'H264PROFILE_SCALABLEBASELINE',
+  8: 'H264PROFILE_SCALABLEHIGH',
+  9: 'H264PROFILE_STEREOHIGH',
+  10: 'H264PROFILE_MULTIVIEWHIGH',
+  11: 'VP8PROFILE_ANY',
+  12: 'VP9PROFILE_PROFILE0',
+  13: 'VP9PROFILE_PROFILE1',
+  14: 'VP9PROFILE_PROFILE2',
+  15: 'VP9PROFILE_PROFILE3',
+  16: 'HEVCPROFILE_MAIN',
+  17: 'HEVCPROFILE_MAIN10',
+  18: 'HEVCPROFILE_MAIN_STILL_PICTURE',
+  19: 'DOLBYVISION_PROFILE0',
+  20: 'DOLBYVISION_PROFILE4',
+  21: 'DOLBYVISION_PROFILE5',
+  22: 'DOLBYVISION_PROFILE7',
+  23: 'THEORAPROFILE_MIN',
+  24: 'AV1PROFILE_PROFILE0',
+}
+
 protobuf.load('video_decode_stats.proto').then(async root => {
   const DecodeStatsProto = root.lookupType('media.DecodeStatsProto')
 
   // Let's read the database and bail out if there's nothing to write.
   if (!args.file) {
+    console.log(['profileId', 'profile', 'resolution', 'frameRate', 'framesDecoded', 'framesDropped', 'framesDecodedPowerEfficient'].join('\t'))
     db.createReadStream().on('data', data => {
-      console.log(data.key.toString(), ' : ', DecodeStatsProto.decode(data.value))
+      const [profileId, resolution, frameRate] = data.key.toString().split('|')
+      const value = DecodeStatsProto.decode(data.value)
+      const row = [
+        profileId,
+        profileId in profiles ? profiles[profileId] : 'VIDEO_CODEC_PROFILE_UNKNOWN',
+        resolution,
+        frameRate,
+        value.framesDecoded.low,
+        value.framesDropped.low,
+        value.framesDecodedPowerEfficient.low
+      ]
+      console.log(row.join('\t'))
     })
     return;
   }
